5211faff68f9f8777984693f363ce6af718a4c73
ElizbethNovakova <lizochka1211@gmail.com>
2021-04-07 20:44:16 +0300

    created project, added dependencies

---
+++ src/main/java/com/novakova/project/BankExpenceApplication.java
@@ -0,0 +1,12 @@
+package com.novakova.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BankExpenceApplication extends SpringBootServletInitializer{
+    public static void main(String[] args) {
+        SpringApplication.run(BankExpenceApplication.class, args);
+    }
+}
